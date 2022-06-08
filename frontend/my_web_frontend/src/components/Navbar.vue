f396545cf824e14fc28ec6f37a405f282b4b59ef
Erichy-dev <erichnyaga1@gmail.com>
2022-06-08 15:28:27 +0300

    set up a simple frontend

---
+++ frontend/my_web_frontend/src/components/Navbar.vue
@@ -0,0 +1,111 @@
+<script setup lang="ts">
+function toggleMenu(e: any) {
+  const droppedMenu = document.getElementById("nav-content");
+  const menuClass = droppedMenu?.classList;
+  const navClass = document.getElementById("navbar")?.classList
+  if (menuClass?.contains("hidden")) {
+    menuClass.remove("hidden");
+    navClass?.remove("max-h-20")
+  } else {
+    menuClass?.add("hidden");
+    navClass?.add("max-h-20")
+  }
+}
+
+
+function dropProjects(){
+  const Projects = document.getElementById("project-dropdown")
+  const projectsClass = Projects?.classList
+  if (projectsClass?.contains("hidden")) {
+    projectsClass.remove("hidden")
+  } else {
+    projectsClass?.add("hidden")
+  }
+}
+
+</script>
+
+<template>
+  <!-- z-30 -->
+  <nav
+    id="navbar"
+    class="bg-gray-800 fixed w-full top-0 text-indigo-400 shadow bg-cover bg-fixed py-1 max-h-20"
+  >
+    <div
+      class="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-2 lg:py-6"
+    >
+      <div class="pl-4 flex items-center">
+        <a
+          class="text-white no-underline hover:no-underline font-bold text-2xl lg:text-4xl"
+          href="#"
+        >
+          <!-- <svg
+            class="h-6 w-6 inline-block fill-current text-yellow-700"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 20 20"
+          >
+            <path d="M13 8V0L8.11 5.87 3 12h4v8L17 8h-4z" />
+          </svg> -->
+          DEVRIKI
+        </a>
+      </div>
+
+      <div class="block lg:hidden pr-4">
+        <button
+          id="nav-toggle"
+          class="flex items-center px-3 py-2 border rounded text-gray-500 border-gray-600 hover:text-gray-800 hover:border-green-500 appearance-none focus:outline-none"
+          @click="toggleMenu"
+        >
+          <!-- <svg
+            class="fill-current h-3 w-3"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          > -->
+          <title class="fill-current h-3 w-3">MENU</title>
+          <!-- <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+          </svg> -->
+        </button>
+      </div>
+
+      <div
+        class="w-full flex-grow lg:flex lg:items-center lg:w-auto hidden mt-2 lg:mt-0 text-orange-900 p-4 lg:p-0 z-20 font-bold"
+        id="nav-content"
+      >
+        <ul
+          id="dropdown"
+          class="list-reset lg:flex justify-end flex-1 items-center"
+        >
+          <li class="mr-3">
+            <span
+              class="inline-block hover:text-green-900 py-2 px-4"
+              @click="dropProjects"
+              >PROJECTS</span
+            >
+            <div
+              id="project-dropdown"
+              class="bg-gray-100 hidden bg-fixed shadow text-red-800 fixed pt-3 px-2"
+            >
+              <ul>
+                <li>VueJs</li>
+                <li>Express&NodeJs</li>
+                <li>ThreeJs</li>
+                <li>SQL</li>
+              </ul>
+            </div>
+          </li>
+          <li class="mr-3">
+            <a class="inline-block hover:text-green-900 py-2 px-4" href="#"
+              >WRITING</a
+            >
+          </li>
+          <li class="mr-3">
+            <a class="inline-block hover:text-green-900 py-2 px-4" href="#"
+              >REWIND&FORESHADOW</a
+            >
+          </li>
+          <li class="inline-block hover:text-green-900 py-2 px-4">ABOUT</li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
--- frontend/my_web_frontend/src/views/HomeView.vue
+++ frontend/my_web_frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
-import TheWelcome from '@/components/TheWelcome.vue'
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <section
+    class="lg:items-start pt-40 px-56 flex flex-wrap flex-col w-full items-center justify-between md:flex-row bg-slate-700 bg-cover font-extrabold font-serif text-4xl md:text-8xl"
+  >
+    WELCOME
+  </section>
+
 </template>
